5dbb031c4c4568f550b853fddfb0d0b44fac5b09
Weidali <vito.co@ya.ru>
2025-04-19 01:05:46 +0300

    - Базовая структура проекта и конфигурация

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $file_composer = json_decode(file_get_contents(base_path('composer.json')), true);
+    $app_name = config('app.name');
+    $app_version = $file_composer['version'];
+
+    $view = '<pre>' . $app_name . ' - '. $app_version . '</pre>';
+    return $view;
 });
